4af773c9f4e30dcd75507f8257841fbd4ab6ec08
Ashdeep04 <singhashy9@gmail.com>
2021-05-11 20:17:54 +0530

    Exercise

--- app/src/main/java/com/example/androidproject/Main.kt
+++ app/src/main/java/com/example/androidproject/Main.kt
@@ -1,5 +1,43 @@
 package com.example.androidproject
 
 fun main(){
-    print("Welcome Kotlin!!")
+    println("Welcome Kotlin!!")
+    /*
+    var myName:String = "Ash"
+    var num = 10
+    var name = "Ashu"
+    var myNum:Int = 69
+    num = 100
+    println("Hello $myNum")
+    println("Hii "+num)
+    val numm = 20 //cannot be changed
+    println("Hell "+numm)
+
+    val myInt = 4_5
+    var myDouble:Double = 2.8
+    println("Hey "+myInt)
+    println("Heyyo "+myDouble)
+    var myByte:Byte = 127 //if not mentioned, by default int will be taken
+    println("Hi "+myByte)
+
+    var myBoolean:Boolean = true
+    println("You are "+myBoolean)
+    var numbers = arrayOf(1,2,3,4,5)
+    var animals = arrayOf("cat","dog","bat")
+    var mixed = arrayOf("cat",1,"bat",true,'a')
+
+    var nums = arrayOf(1,2,3,4,5,6)
+    println("${nums.size}")
+
+*/
+    var myString:String = "Android Kotlin"
+    var myFloat:Float = 13.37f
+    val myDouble:Double = 3.14159265358979
+    var myInt:Int = 27
+
+    println(myString)
+    println(myFloat)
+    println(myDouble)
+    println(myInt)
+
 }
