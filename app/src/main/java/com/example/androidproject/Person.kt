45a03d855bcdc3c6d42e226faf11f2ad9768480c
Ashdeep04 <singhashy9@gmail.com>
2021-06-14 18:53:34 +0530

    getter and setter

---
+++ app/src/main/java/com/example/androidproject/Person.kt
@@ -0,0 +1,12 @@
+package com.example.androidproject
+
+class Person {
+    var name:String? = null
+    set(value){
+        field = value?.toUpperCase()
+    }
+    var gender:String? = null
+    var age:Int = 0
+    set(value) = if(value<18) field = 0 else field=value
+
+}
